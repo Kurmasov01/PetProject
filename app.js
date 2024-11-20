6ee9ecbb73862c253c9bec5886d9068b2221e5c9
kurmasov01 <kurmasov01@mail.ru>
2024-11-20 19:29:51 +0300

    основы 2

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const App = {
     data() {
         return {
-            counter: 0
+            title: 'Список заметок',
+            notePlaceholder: 'Введите название заметки',
+            inputValue: '',
+            notes: []
         }
-    }
+    },
+    methods: {
+        changeInput() {
+            console.log(this.inputValue)
+        },
+        addNote(){
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+        },
+        deleteNote(index) {
+            this.notes.splice(index, 1)
+        }
+    },
 }
 const app = Vue.createApp(App)
 app.mount('#app')
